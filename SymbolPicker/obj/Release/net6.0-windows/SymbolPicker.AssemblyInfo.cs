8a6a97cdaf4bff860cd201201dc15f6f53a2b886
1234567Yang <yang2906455468@gmail.com>
2025-03-23 14:16:19 -0700

    more symbols

--- SymbolPicker/obj/Release/net6.0-windows/SymbolPicker.AssemblyInfo.cs
+++ SymbolPicker/obj/Release/net6.0-windows/SymbolPicker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SymbolPicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5adf0b0bcce74e2b30bbe759c7307ca446b4882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b7e1e30aba9709dba306fad35e8ec6bf627fcd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SymbolPicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SymbolPicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
